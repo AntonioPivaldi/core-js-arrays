92848aba025165f20d01d781559922ce5a58ae63
AntonioPivaldi <9212565@gmail.com>
2024-01-30 00:17:49 +0300

    feat: finish last task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -529,8 +529,20 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let result = 1;
+
+  nums.reduce((acc, el, index) => {
+    if (nums[index + 1] > el) {
+      return acc + 1;
+    }
+
+    result = Math.max(result, acc);
+
+    return 1;
+  }, 1);
+
+  return result;
 }
 
 /**
